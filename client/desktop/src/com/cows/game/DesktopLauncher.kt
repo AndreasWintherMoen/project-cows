2acf1cb44765fcc339084227c903af06056f60e3
pmAllport <patrick.m.allport@gmail.com>
2022-03-10 11:30:50 +0100

    Changed files to kotlin

---
+++ client/desktop/src/com/cows/game/DesktopLauncher.kt
@@ -0,0 +1,15 @@
+package com.cows.game
+
+import kotlin.jvm.JvmStatic
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = Lwjgl3ApplicationConfiguration()
+        config.setForegroundFPS(60)
+        Lwjgl3Application(Application(), config)
+    }
+}
