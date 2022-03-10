45168b07b62ee32a4b41960b415f60704626af1d
pmAllport <patrick.m.allport@gmail.com>
2022-03-10 11:15:44 +0100

    Added libgdx

---
+++ client/android/src/com/cows/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.cows.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.cows.game.Application;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Application(), config);
+	}
+}
