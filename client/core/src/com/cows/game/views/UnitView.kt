2453eb483b4c750f6fd1fedcbc46ee602fae2125
pmAllport <patrick.m.allport@gmail.com>
2022-03-15 12:42:43 +0100

    Added Updatable and updater

--- client/core/src/com/cows/game/views/UnitView.kt
+++ client/core/src/com/cows/game/views/UnitView.kt
@@ -1,7 +1,16 @@
 package com.cows.game.views
 
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.cows.game.models.UnitModel
 
-class UnitView (val model: UnitModel) {
-    // TODO: Implement this like the TowerView
+class UnitView (val model: UnitModel): Renderable() {
+
+    override fun render(batch: SpriteBatch, deltaTime: Float) {
+        TODO("Implement function")
+    }
+
+    override fun dispose() {
+      TODO("Implement function")
+    }
 }
