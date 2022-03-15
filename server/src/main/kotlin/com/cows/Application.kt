4db8122f1931da8010481d2ce6d5608da4044262
pmAllport <patrick.m.allport@gmail.com>
2022-03-15 17:59:26 +0100

    Initial Ktor setup

---
+++ server/src/main/kotlin/com/cows/Application.kt
@@ -0,0 +1,17 @@
+package com.cows
+
+import io.ktor.server.application.*
+import com.cows.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSecurity()
+    configureHTTP()
+    configureMonitoring()
+    configureSerialization()
+    configureSockets()
+}
