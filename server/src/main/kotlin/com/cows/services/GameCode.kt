ad54bdc5eb02cec6670a615fc7c88c4a1524b57c
Jonathan Brooks <brooks.jonathan1@gmail.com>
2022-03-21 15:32:27 +0100

    I am bad at git🧌

--- server/src/main/kotlin/com/cows/services/GameCode.kt
+++ server/src/main/kotlin/com/cows/services/GameCode.kt
@@ -2,7 +2,6 @@ package com.cows.services
 
 import io.ktor.util.date.*
 
-class GameCode() {
-    val timeOfCreation: Long = getTimeMillis()
-    val gameCode: Int = (0..99999).random()
+class GameCode(val gameCodeId:Int,val creator:ClientConnection ) {
+    val timeOfCreation:Long = getTimeMillis()
 }
