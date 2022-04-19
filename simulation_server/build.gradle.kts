cbff176a4fc6865555eb124188a1ef02bd331ceb
Andreas Winther Moen <andreas@winthermoen.no>
2022-04-19 12:47:00 +0200

    Set ktor EngineMain as MainClass

--- simulation_server/build.gradle.kts
+++ simulation_server/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 group = "projectcows"
 version = "0.0.1"
 application {
-    mainClass.set("projectcows.ApplicationKt")
+    mainClass.set("io.ktor.server.netty.EngineMain")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
