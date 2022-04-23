e0e1723e50861273a6dbf595d77d53dbe2d534ec
Andreas Winther Moen <andreas@winthermoen.no>
2022-04-23 21:12:49 +0200

    Append IDs to attack and defend instructions

--- simulation_server/src/main/kotlin/projectcows/simulationModels/RoundSimulator.kt
+++ simulation_server/src/main/kotlin/projectcows/simulationModels/RoundSimulator.kt
@@ -11,6 +11,9 @@ class RoundSimulator {
     fun simulate(defendInstruction : List<JsonTower>, attackInstruction : List<JsonUnit>, path: List<IntArray>) : JsonRoundSimulation {
         val eventLog = mutableListOf<JsonTick>();
 
+        val attackInstructionsWithId = attackInstruction.mapIndexed {index, unit -> JsonUnit(index + 100, unit.type, unit.level, unit.movementSpeed, unit.health )}
+        val defendInstructionsWithId = defendInstruction.mapIndexed {index, tower -> JsonTower(index, tower.type, tower.level, tower.position, tower.range, tower.timeBetweenAttacks, tower.damage)}
+
         val units = attackInstruction.mapIndexed{index, unit -> UnitStatsMapper.jsonUnitToSimulationModel(index, unit) }
         println("units!!!")
         println(units)
@@ -50,7 +53,7 @@ class RoundSimulator {
                 gameOver = true
             }
         }
-        return JsonRoundSimulation(defendInstruction, attackInstruction, eventLog, attackerWon)
+        return JsonRoundSimulation(defendInstructionsWithId, attackInstructionsWithId, eventLog, attackerWon)
     }
 
     private fun win() {
