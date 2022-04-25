b383eb0ac0fc8edcf4c98f8cee93bdfb9598f443
Jonathan Brooks <brooks.jonathan1@gmail.com>
2022-04-25 12:56:31 +0200

    Choose a map at random for each new game (#104)
    * Make Map a class, and generate new map with new games
    
    * Add fun maps:)
    
    * Cleaning🫧
    
    * Add checks incase maps does not exist

--- server/src/main/kotlin/com/cows/Application.kt
+++ server/src/main/kotlin/com/cows/Application.kt
@@ -6,12 +6,10 @@ import com.cows.plugins.*
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
+
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
     configureRouting()
     configureSecurity()
-    /*configureHTTP()*/
-    /*configureMonitoring()*/
-    /*configureSerialization()*/
     configureSockets()
 }
